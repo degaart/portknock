bf7a842c7aba0c57361e7fc139c9ecea7f382f8a
degaart <degaart@users.noreply.github.com>
2026-04-22 17:24:24 +0300

    chore: CI pipeline (wip)

---
+++ .github/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -eou pipefail
+
+cat <<EOF > /etc/apt/sources.list
+deb http://archive.debian.org/debian buster main contrib non-free
+deb http://archive.debian.org/debian buster-updates main contrib non-free
+deb http://archive.debian.org/debian-security/ buster/updates main contrib non-free
+EOF
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get upgrade -qy
+apt-get install -qy build-essential curl
+
+curl https://sh.rustup.rs -sSf|sh -s -- -y
+
+cargo build --release
+
